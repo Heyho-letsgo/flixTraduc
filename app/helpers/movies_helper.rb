8994a1edce20331967cf75556732c2b4de38dcae
André Baudin-Laurencin <abaudin@gmail.com>
2014-04-28 21:55:51 +0200

    Rajout des locales

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,14 +1,18 @@
 module MoviesHelper
 	def format_chiffre_affaire(m)
 		if m.chiffre_affaire < 500000000
-			content_tag(:strong, "Flop but #{number_to_currency(m.chiffre_affaire,unit: "€", separator: ",", delimiter: "", format: "%n %u")}")
+			content_tag(:strong, "Flop")
 		else
-		number_to_currency(m.chiffre_affaire,unit: "€", separator: ",", delimiter: "", format: "%n %u")
+		#content_tag(:strong, "Supeer")
+		number_to_currency(m.chiffre_affaire)#,unit: "€", separator: ",", delimiter: "", format: "%n %u")
+		#content_tag(:strong, "Supeer #{number_to_currency(m.chiffre_affaire,unit: "€", separator: ",", delimiter: "", format: "%n %u")}")
+	
 		end
 	end
 
-	def format_description(m)
-		truncate(m.description,length:120) { link_to "Continue", "#" }# highlight(m.description,'Tony')
 
+	def format_description(m)
+		highlight truncate(m.description, length: 45, separator: ' ') { link_to "Continue", "#" }, 'Tony'
 	end
 end
+		
