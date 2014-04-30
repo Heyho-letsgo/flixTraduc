2d5697b208603c4c7e82448ef0d52a0530822e31
André Baudin-Laurencin <abaudin@gmail.com>
2014-04-30 13:54:33 +0200

    ajout de la page show

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,6 +3,10 @@ class MoviesController < ApplicationController
 	def index
 	 #@movies = ['Iron Man', 'Superman', 'Spider-Man']	
 	@movies = Movie.all
-	@items = Movie.select("id")
-	end
+	#@items = Movie.select("id")
+  end
+
+  def show
+    @movie=Movie.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Flix::Application.routes.draw do
 
-
 get     "movies"     =>     "movies#index"
+get     "movies/:id" =>     "movies#show"
+
  end
